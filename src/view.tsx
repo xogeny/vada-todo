398780c6a60d3367cec43b7637ef8e4cdd87fd22
Michael Tiller <michael.tiller@gmail.com>
2016-01-12 12:24:20 -0500

    Starting view

--- src/view.tsx
+++ src/view.tsx
@@ -1,8 +1,14 @@
-//import React = require('react');
+import React = require('react');
 import ReactDOM = require('react-dom');
 
 //import { } from 'vada-react';
 //import { } from 'vada-browser';
+
+export class App extends React.Component<{}, void> {
+    render() {
+        return <span>Hello</span>;
+    }
+}
 
 export function render(elem: Element) {
     ReactDOM.render(<App/>, elem);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+    entry: "./src/index.ts",
+    resolve: {
+        root: [__dirname],
+        moduleDirectories: ["node_modules"],
+	extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: "/dist/",
+        filename: "./bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+		test: /\.tsx$/,
+		loader: 'ts-loader'
+	    },
+            {
+		test: /\.ts$/,
+		loader: 'ts-loader'
+	    },
+        ]
+    }
+}
