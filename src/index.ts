7aa11c3cf9a76f203ef57cc07bdf3e854d5e17cb
Michael Tiller <michael.tiller@gmail.com>
2016-02-26 14:57:59 -0500

    Added angular dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import 'zone.js';
+import 'reflect-metadata';
+
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
+
 import app = require('./app');
+
+import {bootstrap}    from 'angular2/platform/browser'
+
+import { AppComponent } from './ang';
 
 import view = require('./view');
 
-view.render(document.getElementById("main"))
+const angular = true
+
+if (angular) {
+    console.log("Bootstrap!");
+    bootstrap(AppComponent);
+} else {
+    view.render(document.getElementById("main"))
+}
